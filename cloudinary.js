c829853e672495e5fbf130ead8a862c0950f4a33
mckajua <laob6505@gmail.com>
2025-08-06 20:59:12 +0300

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,25 +1,18 @@
 // cloudinary.js
-export async function uploadToCloudinary(file) {
-  const url = "https://api.cloudinary.com/v1_1/djtgxxcjf/auto/upload";
+export function uploadToCloudinary(file, callback) {
+  const url = "https://api.cloudinary.com/v1_1/djtgxxcjf/upload";
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", "imaroro_preset");
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: formData
+  fetch(url, {
+    method: "POST",
+    body: formData
+  })
+    .then(res => res.json())
+    .then(data => callback(data.secure_url))
+    .catch(err => {
+      alert("Upload failed");
+      console.error(err);
     });
-
-    const data = await response.json();
-
-    if (data.secure_url) {
-      return data.secure_url;
-    } else {
-      throw new Error("Upload failed: " + JSON.stringify(data));
-    }
-  } catch (error) {
-    console.error("Cloudinary Upload Error:", error);
-    return null;
-  }
 }
