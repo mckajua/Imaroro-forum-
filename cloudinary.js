816a4d5d5b8915cdbc1317eb9fd1ebaa018fe9e2
mckajua <laob6505@gmail.com>
2025-08-06 20:40:38 +0300

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,10 +1,9 @@
+// cloudinary.js
 export async function uploadToCloudinary(file) {
   const url = "https://api.cloudinary.com/v1_1/djtgxxcjf/auto/upload";
-
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", "imaroro-preset");
-  formData.append("folder", "samples/ecommerce");
+  formData.append("upload_preset", "imaroro_preset");
 
   try {
     const response = await fetch(url, {
